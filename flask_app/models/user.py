b11b034ab7b3edf1fcebb395da6ea1306f4a6b2b
Lisa <lisa.hl.chen@gmail.com>
2022-06-23 10:11:28 -0700

    correcting db

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -5,7 +5,7 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 from flask import flash
 
 class User:
-    db = "wall_db"
+    db = "login_and_registration"
     def __init__(self,data):
         self.id = data['id']
         self.first_name = data['first_name']
